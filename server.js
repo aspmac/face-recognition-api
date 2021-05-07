943e0d4a0fc980c34965fd1b1ddbff29682168da
aspmac <aniketsen2723@gmail.com>
2021-05-07 23:28:00 +0530

    added original files

---
+++ server.js
@@ -0,0 +1,121 @@
+import express from 'express';
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
+import knex from 'knex';
+
+const db = knex({
+	client: 'pg',
+	connection: {
+	    host : '127.0.0.1',
+	    user : 'postgres',
+	    password : 'awesomecool!@',
+	    database : 'face-recognition-database'
+	}
+});
+
+// db.select('*').from('users').then(data=>{
+// 	console.log(data);
+// });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{ 
+	res.send('success');
+})
+
+app.post('/signin', (req,res)=>{
+	if(!email || !name || !password){
+		return res.status(400).json('incorrect form submission');
+	}
+	db.select('email','hash').from('login')
+		.where('email','=',req.body.email)
+		.then(data=>{
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if(isValid){
+				return db.select('*').from('users')
+				  .where('email','=',req.body.email)
+				  .then(user=>{
+				  	res.json(user[0])
+				  })
+				  .catch(err=>res.status(400).json('unable to get user'))
+			} else{
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err=>res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req,res)=>{
+	const {email, name, password} = req.body;
+	if(!email || !name || !password){
+		return res.status(400).json('incorrect form submission');
+	}
+	const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail=>{
+				return trx('users')
+					.returning('*')
+					.insert({
+						email:loginEmail[0],
+						name:name,
+						joined: new Date()
+					})
+					.then(user=>{
+						res.json(user[0]);
+					})
+				})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})
+		.catch(err=>res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id', (req, res)=>{
+	const{ id } = req.params;
+	// let found= false;
+	db.select('*').from('users').where({id:id})
+	.then(user=>{
+		if(user.length){
+			res.json(user[0]);
+		} else{
+			res.status(400).json('Not found')
+		}		
+	})
+	.catch(err=>res.status(400).json('error getting user'))
+	// if(!found){
+	// 	res.status(400).json('not found');
+	// }
+})
+
+app.put('/image',(req,res)=>{
+	const{ id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries=>{
+		res.json(entries[0]);
+	})
+	.catch(err=>res.status(400).json('unable to get entries'))
+})
+
+app.listen(3000, ()=>{
+	console.log('app is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/SignIn --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> updated user
+
+*/
