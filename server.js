8669c63d3955bb6efcdd90ad5c408129febc4ddd
aspmac <aniketsen2723@gmail.com>
2021-05-08 18:35:53 +0530

    another change

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import knex from 'knex';
 const db = knex({
 	client: 'pg',
 	connection: {
-	    connectionString : process.env.DATABASE_URL,
-	    ssl:true
+	    connectionString: process.env.DATABASE_URL,
+	    ssl:true,
 	}
 });
 
